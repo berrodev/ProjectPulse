d05a1c38152679ddd7b5aa6eceeade730fd4ca34
Francisco Berroeta <fsberroeta@gmail.com>
2024-11-14 19:26:20 -0300

    refactor: change ID and Name properties to id and name in project and task objects

--- main.js
+++ main.js
@@ -1,23 +1,23 @@
 const projects = [
   {
-    ID: 1,
-    Name: 'Project 1',
+    id: 1,
+    name: 'Project 1',
     startDate: '2024-01-01',
     tasks: [
       {
-        ID: 1,
+        id: 1,
         description: 'Task 1',
         state: 'pending',
         deadLine: '2024-01-15',
       },
       {
-        ID: 2,
+        id: 2,
         description: 'Task 2',
         state: 'active',
         deadLine: '2024-01-20',
       },
       {
-        ID: 3,
+        id: 3,
         description: 'Task 3',
         state: 'completed',
         deadLine: '2024-01-25',
@@ -25,24 +25,24 @@ const projects = [
     ],
   },
   {
-    ID: 2,
-    Name: 'Project 2',
+    id: 2,
+    name: 'Project 2',
     startDate: '2024-02-05',
     tasks: [
       {
-        ID: 1,
+        id: 1,
         description: 'Task 1',
         state: 'pending',
         deadLine: '2024-02-15',
       },
       {
-        ID: 2,
+        id: 2,
         description: 'Task 2',
         state: 'active',
         deadLine: '2024-02-20',
       },
       {
-        ID: 3,
+        id: 3,
         description: 'Task 3',
         state: 'completed',
         deadLine: '2024-02-25',
