2b3fc8b41eb9ea5acb431db0e985ba877083031b
Francisco Berroeta <fsberroeta@gmail.com>
2024-11-14 19:22:38 -0300

    feat: add project and task structure to main.js

--- main.js
+++ main.js
@@ -1,1 +1,46 @@
-console.log('Hello World');
+const projects = [
+  {
+    ID: 1,
+    Name: 'Project 1',
+    startDate: '2024-01-01',
+    tasks: [
+      {
+        ID: 1,
+        description: 'Task 1',
+        state: 'pending',
+      },
+      {
+        ID: 2,
+        description: 'Task 2',
+        state: 'active',
+      },
+      {
+        ID: 3,
+        description: 'Task 3',
+        state: 'completed',
+      },
+    ],
+  },
+  {
+    ID: 2,
+    Name: 'Project 2',
+    startDate: '2024-01-01',
+    tasks: [
+      {
+        ID: 1,
+        description: 'Task 1',
+        state: 'pending',
+      },
+      {
+        ID: 2,
+        description: 'Task 2',
+        state: 'active',
+      },
+      {
+        ID: 3,
+        description: 'Task 3',
+        state: 'completed',
+      },
+    ],
+  },
+];
